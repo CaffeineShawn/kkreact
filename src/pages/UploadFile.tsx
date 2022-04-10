f872992e0e238de8c61942041cf0f29e02523b0e
CaffeineShawn <52215406+CaffeineShawn@users.noreply.github.com>
2022-04-10 17:00:35 +0800

    fix: tailwindcss preflight and upload progress

--- src/pages/UploadFile.tsx
+++ src/pages/UploadFile.tsx
@@ -2,66 +2,52 @@ import { Upload, message } from 'antd'
 import { InboxOutlined } from '@ant-design/icons'
 import React from 'react'
 import axios from 'axios'
-import * as events from 'events'
 
-const { Dragger } = Upload
+const uploadUrl = 'https://api.szlikeyou.com:8964/excel/upload'
+const testUrl = 'https://localhost:8080/upload/file'
 
-const uploadExcelHandler = async (options: any) => {
-  const { onSuccess, onError, file, onProgress } = options
-  const formData = new FormData()
-  const uploadUrl = 'https://api.szlikeyou.com:8964/excel/upload'
-  const testUrl = 'https://localhost:8080/upload/file'
-  const config = {
-    headers: {
-      Authorization: 'Bearer ' + localStorage.getItem('token')
-    },
-    mode: 'cors',
-    onUploadProgress: ({ total, loaded }: any) => {
-      onProgress(
-        { percent: Math.round((loaded / total) * 100).toFixed(2) },
-        file
-      )
-    }
-  }
-  formData.append('file', file)
-  axios
-    .post(uploadUrl, formData, config)
-    .then((data) => {
-      onSuccess('上传成功')
-      console.log(data)
-    })
-    .catch((err) => {
-      onError('上传失败')
-      console.log(err)
-    })
-}
-
-const draggerProps = {
-  name: 'file',
+const uploadProps = {
   multiple: false,
-  onChange(info: any) {
-    const { status } = info.file
-    const event = info.event
-    if (status !== 'uploading') {
-      console.log(info.file, info.fileList)
+  action: testUrl,
+  headers: {
+    Authorization: 'Bearer ' + localStorage.getItem('token')
+  },
+  onStart(file: { name: any }) {
+    console.log('onStart', file, file.name)
+  },
+  customRequest(options: any) {
+    const { action, onSuccess, onError, onProgress, file, headers } = options
+    const formData = new FormData()
+    if (file) {
+      formData.append('file', file)
     }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`)
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`)
+    console.log(options)
+    axios
+      .post(action, formData, {
+        headers: headers,
+        onUploadProgress: ({ total, loaded }) => {
+          if (total === loaded) {
+            onProgress({ percent: 100 }, file)
+          }
+          onProgress(
+            { percent: Math.round((loaded / total) * 100).toFixed(2) },
+            file
+          )
+        }
+      })
+      .then(({ data: response }) => {
+        onSuccess(response, file)
+        message.success('上传成功')
+      })
+      .catch((err) => {
+        onError(err, file)
+      })
+
+    return {
+      abort() {
+        console.log('upload progress is aborted.')
+      }
     }
-  },
-  onDrop(e: { dataTransfer: { files: any } }) {
-    console.log('Dropped files', e.dataTransfer.files)
-  },
-  onSuccess(res: any, file: any) {
-    console.log('onSuccess', res, file)
-  },
-  onError(err: any) {
-    console.log('onError', err)
-  },
-  onProgress({ percent }: any, file: any) {
-    console.log('onProgress', `${percent}%`, file.name)
   }
 }
 
@@ -69,7 +55,8 @@ const UploadFile = () => {
   return (
     <div className="flex flex-col h-screen items-center justify-center">
       <div className="flex-initial px-2 md:px-0">
-        <Dragger {...draggerProps} customRequest={uploadExcelHandler}>
+        <Upload.Dragger {...uploadProps}>
+          {/* {...draggerProps} customRequest={uploadExcelHandler} */}
           <p className="ant-upload-drag-icon">
             <InboxOutlined />
           </p>
@@ -80,7 +67,7 @@ const UploadFile = () => {
             Support for a single or bulk upload. Strictly prohibit from
             uploading company data or other band files
           </p>
-        </Dragger>
+        </Upload.Dragger>
       </div>
     </div>
   )
