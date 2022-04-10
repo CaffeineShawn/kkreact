f9270453a4dfe4f3db97f8a495ca55f91b07611a
CaffeineShawn <52215406+CaffeineShawn@users.noreply.github.com>
2022-04-10 21:05:00 +0800

    update: prod and dev ssl certs

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,29 @@
-import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 // @ts-ignore
 import fs from 'fs'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      less: {
-        // 支持内联 JavaScript
-        javascriptEnabled: true
-        // 重写 less 变量，定制样式
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  console.log('process.env: ', process.env.VITE_HTTPS_KEY_PATH, process.env.VITE_HTTPS_CER_PATH)
+
+  return defineConfig({
+    css: {
+      preprocessorOptions: {
+        less: {
+          // 支持内联 JavaScript
+          javascriptEnabled: true
+          // 重写 less 变量，定制样式
+        }
       }
-    }
-  },
-  server: {
-    https: {
-      key: fs.readFileSync('./.cert/key.pem'),
-      cert: fs.readFileSync('./.cert/cert.pem')
-    }
-  },
-  plugins: [viteReact()]
-})
+    },
+    server: {
+      https: {
+        key: fs.readFileSync(process.env.VITE_HTTPS_KEY_PATH),
+        cert: fs.readFileSync(process.env.VITE_HTTPS_CER_PATH)
+      }
+    },
+    plugins: [viteReact()]
+  })
+}
